556b8f6616f207b70973b35c873649ea3fef81f1
Yaohui1996 <zggdscutlyh@163.com>
2021-07-17 19:43:57 +0800

    20210717update

---
+++ _02_04_.c
@@ -0,0 +1,13 @@
+// 编写一个程序
+// 要求用户输入一个美元数量
+// 然后显示出增加5%税率后的相应金额
+
+#include<stdio.h>
+
+int main() {
+    float dollar;
+    scanf("%f", &dollar);
+    printf("Enter an amount: %.2f\n", dollar);
+    printf("With tax added: $%.2f\n", dollar * 1.05);
+    return 0;
+}
