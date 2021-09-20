b4561dda208e41dfcf0e68b8fa58c34b25b28433
Yaohui1996 <zggdscutlyh@163.com>
2021-09-20 12:45:27 +0800

    20210920update

--- _10_06_.c
+++ _10_06_.c
@@ -1,32 +1,91 @@
-/**
- **有些计算器（尤其是惠普的计算器）使用逆波兰表示法（Reverse Polish Notation,
- **RPN）来书写数学表达式。
- **在这一表示法中，运算符放置在操作数的后面而不是放在操作数中间。
- **例如，在逆波兰表示法中1+2将表示为1 2 +，而1+2*3将表示为1 2 3 * +。
- **逆波兰表达式可以很方便地用栈求值。
- **算法从左向右读取运算符和操作数，并执行下列步骤。
- **(1) 当遇到操作数时，将其压入栈中。
- **(2) 当遇到运算符时，从栈中弹出它的操作数，执行运算并把结果压入栈中。
- **编写程序对逆波兰表达式求值。操作数都是个位的整数，运算符为+、-、*、/和=。
- **遇到运算符=时，将显示栈顶项，随后清空栈并提示用户计算新的表达式。
- **这一过程持续进行，直到用户输入一个既不是运算符也不是操作数的字符为止：
- **Enter an RPN expression: 1 2 3 * + =
- **Value of expression: 7
- **Enter an RPN expression: 5 8 * 4 9 - / =
- **Value of expression: -8
- **Enter an RPN expression: q
- **如果栈出现上溢，程序将显示消息Expression is too complex并终止。
- **如果栈出现下溢（例如遇到表达式1 2 + +），程序将显示消息Not enough operands in
- **expression并终止。 提示：把10.2节的栈代码整合到你的程序中。 使用scanf(" %c",
- **&ch)读取运算符和操作数。
- */
-// TODO
+// 有些计算器（尤其是惠普的计算器）使用逆波兰表示法（ReversePolish Notation,
+// RPN）来书写数学表达式。
+// 在这一表示法中，运算符放置在操作数的后面而不是放在操作数中间。
+// 例如，在逆波兰表示法中1+2将表示为12 +，而1+2*3将表示为123 * +。
+// 逆波兰表达式可以很方便地用栈求值。
+// 算法从左向右读取运算符和操作数，并执行下列步骤。
+// (1) 当遇到操作数时，将其压入栈中。
+// (2) 当遇到运算符时，从栈中弹出它的操作数，执行运算并把结果压入栈中。
+// 编写程序对逆波兰表达式求值。
+// 操作数都是个位的整数，运算符为+、-、*、/和=。
+// 遇到运算符=时，将显示栈顶项，随后清空栈并提示用户计算新的表达式。
+// 这一过程持续进行，直到用户输入一个既不是运算符也不是操作数的字符为止：
 
+// 如果栈出现上溢，程序将显示消息Expression is too complex并终止。
+// 如果栈出现下溢（例如遇到表达式12 + +），程序将显示消息Notenough operands in
+// expression并终止。 提示：把10.2节的栈代码整合到你的程序中。 使用scanf("%c",
+// &ch)读取运算符和操作数。
+
+#include <stdbool.h> /* C99 only */
 #include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 10
+/* external variables */
+int contents[STACK_SIZE];
+int top = 0;
+void make_empty(void) { top = 0; }
+bool is_empty(void) { return top == 0; }
+bool is_full(void) { return top == STACK_SIZE; }
+void stack_overflow() {
+    printf("Expression is too complex!\n");
+    exit(-1);
+}
+void stack_underflow() {
+    printf("Notenough operands in expression!\n");
+    exit(-1);
+}
+void push(int i) {
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = i;
+}
+int pop(void) {
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+}
+void print_stack() {
+    for (int i = 0; i != STACK_SIZE; ++i) {
+        printf("[index=%d] [value=%d]\n", i, contents[i]);
+    }
+}
 
 int main() {
-    printf("1\n");
-    char ch = 65;
-    printf("%d\n", ch);
+    printf("Enter an RPN expression: ");
+    make_empty();
+    char ch = getchar();
+    while (true) {
+        if (ch >= '0' && ch <= '9') {
+            push(ch - 48);
+        } else if (ch == '+') {
+            int a = pop();
+            int b = pop();
+            push(b + a);
+        } else if (ch == '-') {
+            int a = pop();
+            int b = pop();
+            push(b - a);
+        } else if (ch == '*') {
+            int a = pop();
+            int b = pop();
+            push(b * a);
+        } else if (ch == '/') {
+            int a = pop();
+            int b = pop();
+            push(b / a);
+        } else if (ch == '=') {
+            printf("Value of expression: %d\n", pop());
+        } else if (ch == ' ') {
+            ch = getchar();
+            continue;
+        } else {
+            break;
+        }
+        ch = getchar();
+        // print_stack();
+    }
     return 0;
 }
--- _10_07_.c
+++ _10_07_.c
@@ -1,26 +1,108 @@
-/**
- **编写程序，提示用户输入一个数并显示该数，使用字符模拟七段显示器的效果：
- **Enter a number: 491-9014
- **非数字的字符都将被忽略。
- **在程序中用一个名为MAX_DIGITS的宏来控制数的最大位数，MAX_DIGITS的值为10。如果数中包含的数位大于这个数，多出来的数位将被忽略。
- **提示：使用两个外部数组，一个是segments数组（见第8章的练习题6），用于存储表示数字和段之间对应关系的数据；
- **另一个是digits数组，这是一个3行（因为显示出来的每个数字高度都是3个字符）、MAX_DIGITS×4列（数字的宽度是3个字符，但为了可读性需要在数字之间增加一个空格）的字符数组。
- **编写4个函数：main、clear_digits_array、process_digit和print_digits_array。
- **下面是后3个函数的原型：
- **void clear_digits_array(void);
- **void process_digit(int digit, int position);
- **void print_digits_array(void);
- **clear_digits_array函数在digits数组的所有元素中存储空白字符。
- **process_digit函数把digit的七段表示存储到digits数组的指定位置（位置从0到MAX_DIGITS1）。
- **print_digits_array函数分行显示digits数组的每一行，产生出像示例图那样的输出。
- */
-// TODO
+// 编写程序，提示用户输入一个数并显示该数，使用字符模拟七段显示器的效果：
+// 非数字的字符都将被忽略。
+// 在程序中用一个名为MAX_DIGITS的宏来控制数的最大位数，MAX_DIGITS的值为10。
+// 如果数中包含的数位大于这个数，多出来的数位将被忽略。
+// 提示：使用两个外部数组，一个是segments数组（见第8章的练习题6），用于存储表示数字和段之间对应关系的数据；
+// 另一个是digits数组，这是一个3行（因为显示出来的每个数字高度都是3个字符）、MAX_DIGITS×4列（数字的宽度是3个字符，但为了可读性需要在数字之间增加一个空格）的字符数组。
+// 编写4个函数：main、clear_digits_array、process_digit和print_digits_array。
+// 下面是后3个函数的原型：
+// void clear_digits_array(void);
+// void process_digit(int digit, int position);
+// void print_digits_array(void);
+// clear_digits_array函数在digits数组的所有元素中存储空白字符。
+// process_digit函数把digit的七段表示存储到digits数组的指定位置（位置从0到MAX_DIGITS-1）。
+// print_digits_array函数分行显示digits数组的每一行，产生出像示例图那样的输出。
 
 #include <stdio.h>
 
+#define MAX_DIGITS 10
+
+const int segment[MAX_DIGITS][7] = {
+    {1, 1, 1, 1, 1, 1, 0},  // 0
+    {0, 1, 1, 0, 0, 0, 0},  // 1
+    {1, 1, 0, 1, 1, 0, 1},  // 2
+    {1, 1, 1, 1, 0, 0, 1},  // 3
+    {0, 1, 1, 0, 0, 1, 1},  // 4
+    {1, 0, 1, 1, 0, 1, 1},  // 5
+    {1, 0, 1, 1, 1, 1, 1},  // 6
+    {1, 1, 1, 0, 0, 0, 0},  // 7
+    {1, 1, 1, 1, 1, 1, 1},  // 8
+    {1, 1, 1, 1, 0, 1, 1}   // 9
+};
+char digits[3][MAX_DIGITS * 4];
+
+void clear_digits_array(void);
+void process_digit(int digit, int position);
+void print_digits_array(void);
+
 int main() {
-    printf("1\n");
-    char ch = 65;
-    printf("%d\n", ch);
+    int read_digits[MAX_DIGITS] = {0};
+    printf("Enter a number: ");
+    char ch = getchar();
+    int has_read = 0;
+    while (has_read < MAX_DIGITS) {
+        if (ch == '\n') {
+            break;
+        }
+        if (ch >= '0' && ch <= '9') {
+            read_digits[has_read] = ch - 48;
+            ++has_read;
+        }
+        ch = getchar();
+    }
+
+    // 清理
+    clear_digits_array();
+
+    // 依次处理每一个数字
+    for (int i = 0; i != has_read; ++i) {
+        // printf("%d ", read_digits[i]);
+        process_digit(read_digits[i], i);
+    }
+
+    // 输出
+    print_digits_array();
+
     return 0;
+}
+
+void clear_digits_array() {
+    for (size_t i = 0; i != 3; ++i) {
+        for (size_t j = 0; j != MAX_DIGITS * 4; ++j) {
+            digits[i][j] = ' ';
+        }
+    }
+}
+
+void process_digit(int digit, int position) {
+    if (segment[digit][0] == 1) {
+        digits[0][4 * position + 1] = '_';
+    }
+    if (segment[digit][5] == 1) {
+        digits[1][4 * position] = '|';
+    }
+    if (segment[digit][6] == 1) {
+        digits[1][4 * position + 1] = '_';
+    }
+    if (segment[digit][1] == 1) {
+        digits[1][4 * position + 2] = '|';
+    }
+    if (segment[digit][4] == 1) {
+        digits[2][4 * position] = '|';
+    }
+    if (segment[digit][3] == 1) {
+        digits[2][4 * position + 1] = '_';
+    }
+    if (segment[digit][2] == 1) {
+        digits[2][4 * position + 2] = '|';
+    }
+}
+
+void print_digits_array() {
+    for (int i = 0; i != 3; ++i) {
+        for (int j = 0; j != MAX_DIGITS * 4; ++j) {
+            printf("%c", digits[i][j]);
+        }
+        printf("\n");
+    }
 }
